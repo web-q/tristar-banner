e76be884df400d2780e6390f500e1388814d4cf0
albertkoncz <akos.koncz@gmail.com>
2017-08-21 10:40:22 -0400

    gulp rework

--- gulpfile.js
+++ gulpfile.js
@@ -420,6 +420,7 @@ gulp.task('build', function(callback) {
     'minify-html', 
     'uglify',    
     'compress',
+    'archive',
     callback);
 });
 
@@ -453,13 +454,11 @@ gulp.task('serve', function(callback) {
 gulp.task('ba', function() { runSequence(['check'], ['build'], ['archive']) });
 
 gulp.task('help', function() {  
-  gutil.log('There are 4 basic commands.');
-  gutil.log(gutil.colors.yellow('gulp'), ': for dev use, spins up server w livereload as you edit files');
-  gutil.log(gutil.colors.yellow('gulp build'), ': minifies files from the dev directory into the', gutil.colors.red('dist'), 'directory');
+  gutil.log('There are 3 basic commands.');
+  gutil.log(gutil.colors.yellow('gulp --size 300x250|320x50|728x90'), ': for dev use, spins up server w livereload as you edit files, --size parameter required');
+  gutil.log(gutil.colors.yellow('gulp build [--size 300x250|320x50|728x90]'), ': minifies files from the dev directory into the', gutil.colors.red('dist'), 'directory');
   gutil.log('and creates a zip of these files in', gutil.colors.red('delivery'), 'directory');
-  gutil.log(gutil.colors.yellow('gulp copy'), ': copy the ', gutil.colors.red('dist'), 'directory to a remote folder.');  
-  gutil.log(gutil.colors.yellow('gulp archive'), ': takes files from the ' + gutil.colors.red('dev'), 'directory' + ' plus other important files');
-  gutil.log('and zips them in the', gutil.colors.red('archive'), 'directory for archival purposes. COMING SOON!');
+  gutil.log(gutil.colors.yellow('gulp copy'), ': copy the ', gutil.colors.red('dist'), 'directory to a remote folder.');    
   gutil.log('--------------------------');
 });
 
